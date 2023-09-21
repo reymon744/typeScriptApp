ee95f00cee7951878d4d9f2160f0cfb26d8a76dd
Reymon <ramon.ale.gonzalez@gmail.com>
2023-09-20 23:27:01 -0300

    navbar update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import NavBar from "@/scenes/navbar";
+import { useState } from "react";
+import { SelectedPage } from "./shared/types";
+
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
   return (
-    <>
-      <div className="app">app</div>
-    </>
+    <div className="app bg-gray-20">
+      <NavBar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
   );
 }
 
