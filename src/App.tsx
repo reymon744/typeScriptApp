b62babcebef682307b4c615d6e297b77c903d82b
Reymon <ramon.ale.gonzalez@gmail.com>
2023-09-26 19:41:33 -0300

    Add Benefits and HText

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import NavBar from "@/scenes/navbar";
-import { useEffect, useState } from "react";
-import { SelectedPage } from "./shared/types";
+import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
+import OurClasses from "@/scenes/ourClasses";
+import Benefits from "@/scenes/benefits";
+import ContactUs from "@/scenes/contactUs";
+import Footer from "@/scenes/footer";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "@/shared/types";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -23,12 +27,16 @@ function App() {
 
   return (
     <div className="app bg-gray-20">
-      <NavBar
+      <Navbar
         isTopOfPage={isTopOfPage}
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
       <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
     </div>
   );
 }
